e50a2f77415d8c12dbed6978d132a4a7dbe9f75f
rwilhel <rwilhel@gmail.com>
2017-04-11 15:21:30 -0600

    Test for Enrollment#kindergarten_participation_by_year

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -7,6 +7,12 @@ class EnrollmentTest < Minitest::Test
   def test_enrollment_name
     enrollment = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
 
-    assert_equal "ACADEMY 20", enrollment.name 
+    assert_equal "ACADEMY 20", enrollment.name
+  end
+
+  def test_returns_kindergarten_participation_by_year
+    enrollment = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal({2010 => 0.391, 2011 => 0.353, 2012 => 0.267}, enrollment.kindergarten_participation_by_year)
   end
 end
