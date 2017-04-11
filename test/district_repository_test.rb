c636de971683c816a00219b7be1cf10111be9051
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-10 20:51:45 -0600

    Test DistrictRepository#find_by_name for case insensitivity

--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -11,9 +11,13 @@ class DistrictRepositoryTest < Minitest::Test
         :kindergarten => "./data/Kindergartners in full-day program.csv"
       }
     })
-    district = dr.find_by_name("ACADEMY 20")
 
-    assert_equal "ACADEMY 20", district.name
+    upcase_district = dr.find_by_name("ACADEMY 20")
+    assert_equal "ACADEMY 20", upcase_district.name
+
+    downcase_district = dr.find_by_name("academy 20")
+    assert_equal "ACADEMY 20", downcase_district.name
+
 
     # assert_equal 7, dr.find_all_matching("WE").count
   end
