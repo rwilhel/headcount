cafe753db3485be67d3315a31b61ea110f60dab7
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-11 18:32:06 -0600

    Fix a typo

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -19,6 +19,6 @@ class EnrollmentTest < Minitest::Test
   def test_returns_kindergarten_participation_in_year
     enrollment = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
 
-    assert_equal 0.391, enrollment.test_returns_kindergarten_participation_in_year(2010)
+    assert_equal 0.391, enrollment.kindergarten_participation_in_year(2010)
   end
 end
