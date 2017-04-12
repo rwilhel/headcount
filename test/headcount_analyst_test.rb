b5add62a018c499d0f309bb88ea3a391a1887afa
rwilhel <rwilhel@gmail.com>
2017-04-12 16:26:48 -0600

    Test HeadcountAnalyst#kindergarten_participation_rate_variation

---
+++ test/headcount_analyst_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/headcount_analyst'
+
+class HeadcountAnalystTest < Minitest::Test
+
+  def test_enrollment_analysis_basics
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    ha = HeadcountAnalyst.new(dr)
+    assert_equal 1.126, ha.kindergarten_participation_rate_variation("GUNNISON WATERSHED RE1J", :against => "TELLURIDE R-1")
+    assert_equal 0.447, ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'YUMA SCHOOL DISTRICT 1')
+  end
+end
