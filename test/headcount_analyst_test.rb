5961337e97c1579ff04f1157eddc4275db3fc118
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-13 11:24:56 -0600

    Add assertions to HeadcountAnalyst test
    Also, add link for HeadcountAnalyst test file to test helper

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -11,6 +11,8 @@ class HeadcountAnalystTest < Minitest::Test
     ha = HeadcountAnalyst.new(dr)
 
     assert_equal 1.126, ha.kindergarten_participation_rate_variation("GUNNISON WATERSHED RE1J", :against => "TELLURIDE R-1")
-    assert_equal 0.447, ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'YUMA SCHOOL DISTRICT 1')
+    assert_equal 1.126, ha.kindergarten_participation_rate_variation("gunnison watershed re1j", :against => "telluride r-1")
+    assert_equal 0.766, ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
+    assert_equal 0.766, ha.kindergarten_participation_rate_variation('academy 20', :against => 'colorado')
   end
 end
