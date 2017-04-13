f4da9721373789e4182e214d3195b0f25090e025
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-13 15:08:58 -0600

    Test for EnrollmentRepository with high school data

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -22,4 +22,20 @@ class EnrollmentRepositoryTest < Minitest::Test
     assert_equal "GUNNISON WATERSHED RE1J", enrollment.name
     assert_equal 0.144, enrollment.kindergarten_participation_in_year(2004)
   end
+
+  def test_enrollment_repository_with_high_school_data
+      er = EnrollmentRepository.new
+      er.load_data({
+                     :enrollment => {
+                       :kindergarten => "./data/Kindergartners in full-day program.csv",
+                       :high_school_graduation => "./data/High school graduation rates.csv"
+                     }
+                   })
+      e = er.find_by_name("MONTROSE COUNTY RE-1J")
+
+      assert_equal 0.738, e.graduation_rate_in_year(2010)
+
+      expected = {2010=>0.738, 2011=>0.751, 2012=>0.777, 2013=>0.713, 2014=>0.757}
+      assert_equal expected, e.graduation_rate_by_year
+    end
 end
