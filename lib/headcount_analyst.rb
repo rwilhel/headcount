b76c6ef5c00f6dde0fb6bd74daa40dedfab69b97
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-13 12:00:25 -0600

    Implement HeadcountAnalyst#kindergarten_participation_rate_variation

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -7,10 +7,24 @@ class HeadcountAnalyst
     @district_repository = district_repository
   end
 
-  def kindergarten_participation_rate_variation(district_name, comparison_district)
+  def kindergarten_participation_rate_variation(district_name, comparison_district_hash)
     district_repository.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+
     district = district_repository.find_by_name(district_name)
     enrollment_by_year = district.enrollment.kindergarten_participation_by_year
+    average_participation = get_average(enrollment_by_year)
+    average_participation = (((average_participation*1000).floor).to_f)/1000
+
+    comparison_district = district_repository.find_by_name(comparison_district_hash[:against])
+    comparison_enrollment_by_year = comparison_district.enrollment.kindergarten_participation_by_year
+    comparison_average_participation = get_average(comparison_enrollment_by_year)
+    comparison_average_participation = (((comparison_average_participation*1000).floor).to_f)/1000
+
+    ratio = average_participation / comparison_average_participation
+    ratio = (((ratio*1000).floor).to_f)/1000
+  end
+
+  def get_average(enrollment_by_year)
     sum = 0
     count = 0
     enrollment_by_year.each_value do |value|
