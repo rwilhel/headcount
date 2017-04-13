39e105efc12048bcec4a2db12a7ac9e9c4e95c07
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-13 13:33:45 -0600

    Test for HeadcountAnalyst#kindergarten_participation_rate_variation_trend

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -14,5 +14,8 @@ class HeadcountAnalystTest < Minitest::Test
     assert_equal 1.126, ha.kindergarten_participation_rate_variation("gunnison watershed re1j", :against => "telluride r-1")
     assert_equal 0.766, ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
     assert_equal 0.766, ha.kindergarten_participation_rate_variation('academy 20', :against => 'colorado')
+
+    expected_result = {2004 => 1.257, 2005 => 0.96, 2006 => 1.05, 2007 => 0.992, 2008 => 0.717, 2009 => 0.652, 2010 => 0.681, 2011 => 0.727, 2012 => 0.688, 2013 => 0.694, 2014 => 0.661 }
+    assert_equal expected_result, ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
   end
 end
