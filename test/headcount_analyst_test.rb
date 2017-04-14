118f1e0333b589d1674741dad9d20fa741b83664
rwilhel <rwilhel@gmail.com>
2017-04-14 09:36:57 -0600

    Test for HeadcountAnalyst#kindergarten_participation_against_high_school_graduation

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -18,4 +18,14 @@ class HeadcountAnalystTest < Minitest::Test
     expected_result = {2004 => 1.257, 2005 => 0.96, 2006 => 1.05, 2007 => 0.992, 2008 => 0.717, 2009 => 0.652, 2010 => 0.681, 2011 => 0.727, 2012 => 0.688, 2013 => 0.694, 2014 => 0.661 }
     assert_equal expected_result, ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
   end
+
+  def test_high_school_versus_kindergarten_analysis
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv",
+                                  :high_school_graduation => "./data/High school graduation rates.csv"}})
+    ha = HeadcountAnalyst.new(dr)
+
+    assert_equal 0.548, ha.kindergarten_participation_against_high_school_graduation('MONTROSE COUNTY RE-1J')
+    assert_equal 0.800, ha.kindergarten_participation_against_high_school_graduation('STEAMBOAT SPRINGS RE-2')
+  end
 end
