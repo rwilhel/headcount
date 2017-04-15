444450852d703efb6ba74845c9a6b97242e5ea97
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-15 16:01:39 -0600

    Test for correlation among several districts

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -49,4 +49,13 @@ class HeadcountAnalystTest < Minitest::Test
 
     refute ha.kindergarten_participation_correlates_with_high_school_graduation(:for => 'STATEWIDE')
   end
+
+  def test_kindergarten_hs_prediction_multi_district
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv",
+                                  :high_school_graduation => "./data/High school graduation rates.csv"}})
+    ha = HeadcountAnalyst.new(dr)
+    districts = ["ACADEMY 20", 'PARK (ESTES PARK) R-3', 'YUMA SCHOOL DISTRICT 1']
+    assert ha.kindergarten_participation_correlates_with_high_school_graduation(:across => districts)
+  end
 end
