b691acc4665b47a78c6599f60e08de9874a52d5a
rwilhel <rwilhel@gmail.com>
2017-04-18 16:37:44 -0600

    Test EconomicProfile#median_household_income_average and EconomicProfile#children_in_poverty_in_year

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/economic_profile'
 require './lib/economic_profile_repository'
+require './lib/custom_errors'
 
 class EconomicProfileTest < Minitest::Test
 
@@ -31,6 +32,36 @@ class EconomicProfileTest < Minitest::Test
     economic_profile = epr.find_by_name("ACADEMY 20")
 
     assert_equal 88279, economic_profile.median_household_income_in_year(2010)
-    assert_raises(UnknownDataError){economic_profile.medimedian_household_income_in_year(1)}
+    assert_raises(UnknownDataError){economic_profile.median_household_income_in_year(1)}
+  end
+
+  def test_for_median_household_income_average
+    epr = EconomicProfileRepository.new
+    epr.load_data({
+                    :economic_profile => {
+                      :median_household_income => "./data/Median household income.csv",
+                      :children_in_poverty => "./data/School-aged children in poverty.csv",
+                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                      :title_i => "./data/Title I students.csv"
+                    }
+                  })
+    economic_profile = epr.find_by_name("ACADEMY 20")
+
+    assert_equal 87635, economic_profile.median_household_income_average
+  end
+
+  def test_it_can_find_children_in_poverty_in_given_year
+    epr = EconomicProfileRepository.new
+    epr.load_data({
+                    :economic_profile => {
+                      :median_household_income => "./data/Median household income.csv",
+                      :children_in_poverty => "./data/School-aged children in poverty.csv",
+                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                      :title_i => "./data/Title I students.csv"
+                    }
+                  })
+    economic_profile = epr.find_by_name("ACADEMY 20")
+
+    assert_equal 0.064, economic_profile.children_in_poverty_in_year(2012)
   end
 end
