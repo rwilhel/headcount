147151b24f25f7274063c134359106f142f7eb1c
rwilhel <rwilhel@gmail.com>
2017-04-18 15:34:38 -0600

    Test for EconomicProfile#median_household_income_in_year

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/economic_profile'
+require './lib/economic_profile_repository'
 
 class EconomicProfileTest < Minitest::Test
 
@@ -15,5 +16,20 @@ class EconomicProfileTest < Minitest::Test
     ep = EconomicProfile.new(data)
 
     assert_instance_of EconomicProfile, ep
+  end
+
+  def test_median_household_income_in_year
+    epr = EconomicProfileRepository.new
+    epr.load_data({
+                    :economic_profile => {
+                      :median_household_income => "./data/Median household income.csv",
+                      :children_in_poverty => "./data/School-aged children in poverty.csv",
+                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                      :title_i => "./data/Title I students.csv"
+                    }
+                  })
+    economic_profile = epr.find_by_name("ACADEMY 20")
+
+    assert_equal 88279, economic_profile.median_household_income_in_year(2010)
   end
 end
