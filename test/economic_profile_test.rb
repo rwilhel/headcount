db5a86f5091a8afc71696a8567e3dfd5fa718c48
rwilhel <rwilhel@gmail.com>
2017-04-18 17:17:49 -0600

    Tests that EconomicProfile#title_i_in_year returns percentage

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -100,4 +100,19 @@ class EconomicProfileTest < Minitest::Test
 
     assert_raises(UnknownDataError){economic_profile.free_or_reduced_price_lunch_number_in_year(1)}
   end
+
+  def test_it_can_return_title_i_percentage_as_float
+    epr = EconomicProfileRepository.new
+    epr.load_data({
+                    :economic_profile => {
+                      :median_household_income => "./data/Median household income.csv",
+                      :children_in_poverty => "./data/School-aged children in poverty.csv",
+                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                      :title_i => "./data/Title I students.csv"
+                    }
+                  })
+    economic_profile = epr.find_by_name("ACADEMY 20")
+
+    assert_equal 0.01072, economic_profile.title_i_in_year(2012)
+  end
 end
