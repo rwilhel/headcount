b749de8ed635bac3b1fb57ae330d7ab542e3efbd
rwilhel <rwilhel@gmail.com>
2017-04-18 17:13:52 -0600

    Tests that EconomicProfile#free_or_reduced_price_lunch_number_in_year returns the total as integer

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -65,5 +65,39 @@ class EconomicProfileTest < Minitest::Test
     assert_equal 0.064, economic_profile.children_in_poverty_in_year(2012)
 
     assert_raises(UnknownDataError){economic_profile.children_in_poverty_in_year(1)}
-end
+  end
+
+  def test_it_can_return_free_or_reduced_price_lunch_percentage_in_year
+    epr = EconomicProfileRepository.new
+    epr.load_data({
+                    :economic_profile => {
+                      :median_household_income => "./data/Median household income.csv",
+                      :children_in_poverty => "./data/School-aged children in poverty.csv",
+                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                      :title_i => "./data/Title I students.csv"
+                    }
+                  })
+    economic_profile = epr.find_by_name("ACADEMY 20")
+
+    assert_equal 0.12539, economic_profile.free_or_reduced_price_lunch_percentage_in_year(2012)
+
+    assert_raises(UnknownDataError){economic_profile.free_or_reduced_price_lunch_percentage_in_year(1)}
+  end
+
+  def test_it_can_return_total_on_free_or_reduced_lunch_in_year
+    epr = EconomicProfileRepository.new
+    epr.load_data({
+                    :economic_profile => {
+                      :median_household_income => "./data/Median household income.csv",
+                      :children_in_poverty => "./data/School-aged children in poverty.csv",
+                      :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+                      :title_i => "./data/Title I students.csv"
+                    }
+                  })
+    economic_profile = epr.find_by_name("ACADEMY 20")
+
+    assert_equal 3006, economic_profile.free_or_reduced_price_lunch_number_in_year(2012)
+
+    assert_raises(UnknownDataError){economic_profile.free_or_reduced_price_lunch_number_in_year(1)}
+  end
 end
